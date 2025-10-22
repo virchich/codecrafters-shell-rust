5562937f4ba4e1be07854f26a926ebccba55010e
Ivan Virchich <ivan.virchich@outlook.com>
2025-10-21 19:00:34 -0600

    feat: check if command is allowed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
+    let allowed_commands: [String; 0] = [];
+
     print!("$ ");
     io::stdout().flush().unwrap();
 
     // Wait for user input
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
+
+    if !allowed_commands.contains(&command) {
+        eprintln!("{}: command not found", command.trim());
+        exit(1);
+    }
 }

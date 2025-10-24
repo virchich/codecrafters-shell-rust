966dd57b45e3f0e5ff68fa53fe3f5c835e1b093a
Ivan Virchich <ivan.virchich@outlook.com>
2025-10-24 07:41:41 -0600

    feat: enhance type command to handle PATH variable and check for command existence

--- src/commands/cmd/type_of.rs
+++ src/commands/cmd/type_of.rs
@@ -1,3 +1,5 @@
+use std::env::{split_paths, var};
+use std::path::{Path};
 use crate::commands::command::Command;
 use crate::commands::validator::is_command_allowed;
 
@@ -7,13 +9,40 @@ pub fn type_of(command: &Command) {
         return;
     }
 
-    if !is_command_allowed(command.arguments.first().unwrap()) {
-        eprintln!("{}: not found", command.arguments.first().unwrap());
-        return;
-    } else {
-        println!("{} is a shell builtin", command.arguments.first().unwrap());
+    let command_argument = command.arguments.first().unwrap();
+
+    if is_command_allowed(command_argument) {
+        println!("{} is a shell builtin", command_argument);
         return;
     }
 
-    eprintln!("{}: not found", command.arguments.first().unwrap());
+    match var("PATH") {
+        Ok(path) => {
+            type_path_handler(&command_argument, path);
+            return;
+        }
+        Err(_) => {
+            eprintln!("type: PATH variable not set");
+            return;
+        }
+    }
+}
+
+fn type_path_handler(command: &String, paths: String) {
+    for path in split_paths(&paths) {
+        let dir = Path::new(path.as_path());
+
+        if dir.exists() && dir.is_dir() {
+            for dir_entry in std::fs::read_dir(dir).unwrap() {
+                let entry = dir_entry.unwrap().path();
+
+                if entry.ends_with(command) {
+                    println!("{} is {}", command, entry.display());
+                    return
+                }
+            }
+        }
+    }
+
+    eprintln!("{}: not found", command);
 }

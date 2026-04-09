9e2a0ac843b5b97351335e1b420287d568d699f9
raccoon-353 <>
2026-04-09 12:07:09 -0600

    feat: add history command and implement history management

--- src/commands/built_ins/history.rs
+++ src/commands/built_ins/history.rs
@@ -1,10 +1,24 @@
+use crate::commands::command::Command;
 use crate::repl::history_store;
 use std::io::Write;
 
-pub fn history(output: &mut dyn Write) {
+pub fn history(command: &Command, output: &mut dyn Write) {
     let entries = history_store::get_all();
+    let history_limit = if command.arguments.len() > 0 {
+        match command.arguments[0].parse::<usize>() {
+            Ok(n) => n,
+            Err(_) => {
+                output.write_all(format!("history: {}: numeric argument required", command.arguments[0]).as_bytes()).unwrap();
+                return;
+            }
+        }
+    } else {
+        entries.len()
+    };
 
-    for (i, command) in entries.iter().enumerate() {
+    let history_entries_to_skip: usize = entries.len().saturating_sub(history_limit);
+
+    for (i, command) in entries.iter().enumerate().skip(history_entries_to_skip) {
         writeln!(output, "  {} {}", i + 1, command).unwrap();
     }
 }
--- src/commands/core.rs
+++ src/commands/core.rs
@@ -72,7 +72,7 @@ fn run_pipeline(pipeline: &Pipeline) {
             let mut stderr_writer: Box<dyn Write> = get_redirect(&segment);
 
             match segment.command.command.as_str() {
-                "history" => history(&mut *writer),
+                "history" => history(&segment.command, &mut *writer),
                 "exit" => exit(&segment.command, &mut *stderr_writer),
                 "echo" => echo(&segment.command, &mut *writer),
                 "type" => type_of(&segment.command, &mut *writer, &mut *stderr_writer),
@@ -159,7 +159,7 @@ pub fn run_redirect(redirect_statement: &RedirectStatement) {
     let mut stderr_writer = get_redirect(&redirect_statement);
 
     match redirect_statement.command.command.as_str() {
-        "history" => history(&mut *stdout_writer),
+        "history" => history(&redirect_statement.command, &mut *stdout_writer),
         "exit" => exit(&redirect_statement.command, &mut *stderr_writer),
         "echo" => echo(&redirect_statement.command, &mut *stdout_writer),
         "type" => type_of(

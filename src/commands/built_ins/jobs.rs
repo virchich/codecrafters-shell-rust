cfc1c1cb5babf1e73f1340d1d018de07f9b9cac3
raccoon-353 <>
2026-05-05 12:47:19 -0600

    feat: enhance jobs listing with job order indicators for background processes

--- src/commands/built_ins/jobs.rs
+++ src/commands/built_ins/jobs.rs
@@ -5,7 +5,23 @@ use std::io::Write;
 pub fn jobs(_command: &Command, writer: &mut dyn Write) {
     let jobs_snapshot = jobs_store::snapshot();
 
-    for job in jobs_snapshot.iter() {
-        writeln!(writer, "[{}]+  {:<24}{}", job.id, job.status, job.command).unwrap();
+    for (i, job) in jobs_snapshot.iter().enumerate() {
+        let last_background_job = i + 1 == jobs_snapshot.len();
+        let second_to_last_background_job = i + 1 == jobs_snapshot.len() - 1;
+
+        let job_order: char = if last_background_job {
+            '+'
+        } else if second_to_last_background_job {
+            '-'
+        } else {
+            ' '
+        };
+
+        writeln!(
+            writer,
+            "[{}]{}  {:<24}{}",
+            job.id, job_order, job.status, job.command
+        )
+            .unwrap();
     }
 }

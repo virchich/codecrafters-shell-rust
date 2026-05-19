152f4c8850099e7350d7fd49f2a1ed5fff4214f1
raccoon-353 <>
2026-05-19 15:17:41 -0600

    feat: reap jobs automatically

--- src/commands/built_ins/jobs.rs
+++ src/commands/built_ins/jobs.rs
@@ -25,3 +25,29 @@ pub fn jobs(_command: &Command, writer: &mut dyn Write) {
             .unwrap();
     }
 }
+
+pub fn print_done_jobs(writer: &mut dyn Write) {
+    let jobs_snapshot = jobs_store::list_and_reap();
+
+    for (i, job) in jobs_snapshot.iter().enumerate() {
+        let last_background_job = i + 1 == jobs_snapshot.len();
+        let second_to_last_background_job = i + 1 == jobs_snapshot.len() - 1;
+
+        let job_order: char = if last_background_job {
+            '+'
+        } else if second_to_last_background_job {
+            '-'
+        } else {
+            ' '
+        };
+
+        if job.status == "Done" {
+            writeln!(
+                writer,
+                "[{}]{}  {:<24}{}",
+                job.id, job_order, job.status, job.command
+            )
+                .unwrap();
+        }
+    }
+}
--- src/repl/repl.rs
+++ src/repl/repl.rs
@@ -1,13 +1,17 @@
+use crate::commands::built_ins::jobs::print_done_jobs;
 use crate::commands::core::run_statement;
 use crate::commands::parser::parser::Parser;
 use crate::commands::scanner::lexer::Lexer;
 use crate::repl::editor::get_editor;
 use crate::repl::history_store;
+use std::io;
 
 pub fn repl() {
     let mut editor = get_editor();
 
     loop {
+        print_done_jobs(&mut Box::new(io::stdout()));
+
         let line = editor.readline("$ ");
 
         match line {

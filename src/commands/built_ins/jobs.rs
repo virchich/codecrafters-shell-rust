190e919fba1b17feb6979f72491d1f1ad4f9c8f3
raccoon-353 <>
2026-05-23 15:22:21 -0600

    refactor: deduplicate jobs output formatting

--- src/commands/built_ins/jobs.rs
+++ src/commands/built_ins/jobs.rs
@@ -1,53 +1,47 @@
 use crate::state::jobs_store;
+use crate::state::jobs_store::JobStatus;
 use crate::syntax::command_invocation::CommandInvocation;
 use std::io::Write;
 
 pub fn jobs(_command: &CommandInvocation, writer: &mut dyn Write, _writer_err: &mut dyn Write) {
     let jobs_snapshot = jobs_store::list_and_reap();
 
-    for (i, job) in jobs_snapshot.iter().enumerate() {
-        let last_background_job = i + 1 == jobs_snapshot.len();
-        let second_to_last_background_job = i + 1 == jobs_snapshot.len() - 1;
-
-        let job_order: char = if last_background_job {
-            '+'
-        } else if second_to_last_background_job {
-            '-'
-        } else {
-            ' '
-        };
-
-        writeln!(
-            writer,
-            "[{}]{}  {:<24}{}",
-            job.id, job_order, job.status, job.command
-        )
-        .unwrap();
-    }
+    print_jobs(&jobs_snapshot, writer, |_| true);
 }
 
 pub fn print_done_jobs(writer: &mut dyn Write) {
     let jobs_snapshot = jobs_store::list_and_reap();
 
-    for (i, job) in jobs_snapshot.iter().enumerate() {
-        let last_background_job = i + 1 == jobs_snapshot.len();
-        let second_to_last_background_job = i + 1 == jobs_snapshot.len() - 1;
+    print_jobs(&jobs_snapshot, writer, |job| job.status == "Done");
+}
 
-        let job_order: char = if last_background_job {
-            '+'
-        } else if second_to_last_background_job {
-            '-'
-        } else {
-            ' '
-        };
-
-        if job.status == "Done" {
-            writeln!(
-                writer,
-                "[{}]{}  {:<24}{}",
-                job.id, job_order, job.status, job.command
-            )
-            .unwrap();
+fn print_jobs(
+    jobs_snapshot: &[JobStatus],
+    writer: &mut dyn Write,
+    should_print: impl Fn(&JobStatus) -> bool,
+) {
+    for (index, job) in jobs_snapshot.iter().enumerate() {
+        if should_print(job) {
+            print_job(job, job_order(index, jobs_snapshot.len()), writer);
         }
+    }
+}
+
+fn print_job(job: &JobStatus, job_order: char, writer: &mut dyn Write) {
+    writeln!(
+        writer,
+        "[{}]{}  {:<24}{}",
+        job.id, job_order, job.status, job.command
+    )
+    .unwrap();
+}
+
+fn job_order(index: usize, job_count: usize) -> char {
+    if index + 1 == job_count {
+        '+'
+    } else if index + 1 == job_count.saturating_sub(1) {
+        '-'
+    } else {
+        ' '
     }
 }
